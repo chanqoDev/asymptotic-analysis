654a64c07166aa85f7945ca94b56095527109587
Christian Elena <christianelena92@gmail.com>
2025-08-13 13:23:33 -0600

    add lesson 1 constant time and logmarith time complexity

---
+++ lesson1.js
@@ -0,0 +1,8 @@
+const nemo = ['nemo']; 
+
+function findNemo(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'nemo') return  console.log('found Nemo'); 
+    }
+}
+findNemo(nemo); 
