56f01e573badd3ad4d82537874baf86968b33af0
Christian Elena <christianelena92@gmail.com>
2025-08-14 08:00:29 -0600

    Remove prev project nonessential items

--- routing-demo/src/app/products/[productId]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function ProductDetails({
-    params, 
-}: {
-    params: Promise<{ productId: string }>;
-}) {
-    const productId = (await params).productId; 
-    return <h1>Product {productId} Details</h1>;
-}
